f3579bbe81816bf547bdb9bf4d5dd7e70e664856
Josh Bedo <jbedo@pinch.me>
2014-07-13 20:51:21 -0400

    Adding gulpfile.js and vendors

---
+++ gulpfile.js
@@ -0,0 +1,122 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var livereload = require('gulp-livereload');
+var plumber = require('gulp-plumber');
+var stylus = require('gulp-stylus');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
+
+
+var env = 'development',
+    paths = {
+      src: './app',
+      dest: './public',
+      vendor: './vendor',
+      assets: './assets'
+    };
+
+gulp.task('set-production', function() {
+  env = 'production'
+});
+
+gulp.task('assets', function() {
+  gulp.src(paths.assets + '**')
+    .pipe(plumber())
+    .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('vendor-styles', function() {
+  stream = gulp.src([
+    paths.vendor + 'styles/bootstrap.css',
+    paths.vendor + 'styles/bootstrap-theme.css'
+  ])
+  .pipe(plumber())
+  .pipe(concat('vendor.css'))
+
+  if (env == 'production') {
+    stream.pipe(minify())
+  }
+
+  stream.pipe(gulp.dest(paths.dest + 'css/'))
+});
+
+gulp.task('vendor-scripts', function() {
+  stream = gulp.src([
+    paths.vendor + 'scripts/jquery.js',
+    paths.vendor + 'scripts/bootstrap.js',
+    paths.vendor + 'scripts/underscore.js',
+    paths.vendor + 'scripts/backbone.js',
+    paths.vendor + 'scripts/backbone.syphon.js',
+    paths.vendor + 'scripts/backbone.marionette.js'
+  ])
+  .pipe(plumber())
+  .pipe(concat('vendor.js'))
+
+  if (env == 'production') {
+    stream.pipe(uglify())
+  }
+
+  stream.pipe(gulp.dest(paths.dest + 'js/'))
+});
+
+gulp.task('scripts', function() {
+  stream = gulp.src(paths.src + 'scripts/index.coffee', {read: false})
+    .pipe(plumber())
+    .pipe(browserify({
+      debug: env == 'development',
+      transform: ['coffeeify', 'jadeify'],
+      extensions: ['.coffee', '.jade']
+    }))
+    .pipe(concat('index.js'))
+
+  if (env == 'production') {
+    stream.pipe(uglify())
+  }
+
+  stream.pipe(gulp.dest(paths.dest + 'js/'))
+});
+
+gulp.task('html', function() {
+  gulp.src(paths.src + 'index.jade') 
+    .pipe(plumber())
+    .pipe(jade({
+      pretty: env == 'development'
+    }))
+    .pipe(gulp.dest(paths.dest))
+});
+
+gulp.task('styles', function() {
+  stream = gulp.src(paths.src + 'styles/**/*.styl')
+    .pipe(plumber())
+    .pipe(stylus({ use: ['nib'] }))
+
+  if (env == 'production') {
+    stream.pipe(minify())
+  }
+
+  stream.pipe(gulp.dest(paths.dest + 'css/'))
+});
+
+gulp.task('watch', function() {
+  var server = livereload();
+
+  gulp.watch(paths.src + 'scripts/**', ['scripts']);
+  gulp.watch(paths.src + 'styles/**/*.styl', ['styles']);
+  gulp.watch(paths.src + 'index.jade', ['html']);
+
+  gulp.watch([
+    paths.dest + 'js/*.js',
+    paths.dest + 'css/*.css',
+    paths.dest + '**/*.html'
+  ], function(ev) {
+    server.changed(ev.path);
+  });
+});
+
+gulp.task('vendor', ['vendor-styles', 'vendor-scripts']);
+gulp.task('compile', ['html', 'styles', 'scripts']);
+
+gulp.task('default', ['assets', 'vendor', 'compile']);
+gulp.task('production', ['set-production', 'default']);
